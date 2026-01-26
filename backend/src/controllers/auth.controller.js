3829304d28b084a243713e5ba385ea9324496e42
Ragul <ragulragul8788@gmail.com>
2026-01-26 19:51:59 +0530

    fixed cookie issue that caused issue in production

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -29,7 +29,7 @@ export const signup = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true, // Can't be accessed by JavaScript
       secure: process.env.NODE_ENV === "production", // Only HTTPS in production
-      sameSite: "strict", // CSRF protection
+      sameSite: "lax", // CSRF protection
       maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
     });
     res.json({ message: "Sign up successful" });
@@ -58,7 +58,7 @@ export const signin = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "lax",
       maxAge: 24 * 60 * 60 * 1000,
     });
     res.json({ message: "Sign up successful" });
